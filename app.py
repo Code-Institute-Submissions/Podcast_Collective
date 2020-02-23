f6b573ccff1d0593f2da5b35363bac25bccfa228
Mark Dyson <mark.dyson1@outlook.com>
2020-02-23 20:14:30 +0000

    code tidy

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'podcasts'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_landing')
@@ -16,30 +16,36 @@ def get_landing():
     return render_template("landing.html",
                            podcasts=mongo.db.podcasts.find())
 
+
 @app.route('/get_manage')
 def get_manage():
     return render_template("manage.html",
                            podcasts=mongo.db.podcasts.find())
+
 
 @app.route('/get_contact')
 def get_contact():
     return render_template("contact.html",
                            podcasts=mongo.db.podcasts.find())
 
+
 @app.route('/get_sent')
 def get_sent():
     return render_template("form_sent.html",
                            podcasts=mongo.db.podcasts.find())
+
 
 @app.route('/delete_podcast/<podcast_id>')
 def get_delete(podcast_id):
     mongo.db.podcasts.remove({'_id': ObjectId(podcast_id)})
     return redirect(url_for('get_manage'))
 
+
 @app.route('/get_add')
 def get_add():
     return render_template("add.html",
                            podcasts=mongo.db.podcasts.find())
+
 
 @app.route('/get_gatekeeper')
 def get_gatekeeper():
@@ -54,7 +60,6 @@ def insert_podcast():
     return redirect(url_for('get_manage'))
 
 
-
 @app.route('/edit_podcast/<podcast_id>')
 def get_edit(podcast_id):
     the_podcast = mongo.db.podcasts.find_one({"_id": ObjectId(podcast_id)})
@@ -67,17 +72,18 @@ def get_edit(podcast_id):
 def get_update(podcast_id):
     podcasts = mongo.db.podcasts
     podcasts.update({'_id': ObjectId(podcast_id)},
-    {
-        'podcast_name':request.form.get('podcast_name'),
+                    {
+        'podcast_name': request.form.get('podcast_name'),
         'podcast_description': request.form.get('podcast_description'),
-        'podcast_image':request.form.get('podcast_image'),
-        'podcast_website':request.form.get('podcast_website'),
+        'podcast_image': request.form.get('podcast_image'),
+        'podcast_website': request.form.get('podcast_website'),
     })
     return redirect(url_for('get_manage'))
 
+
 @app.route('/podcast_details/<podcast_id>')
 def get_details(podcast_id):
-    the_podcast= mongo.db.podcasts.find_one({'_id': ObjectId(podcast_id)})
+    the_podcast = mongo.db.podcasts.find_one({'_id': ObjectId(podcast_id)})
     return render_template("podcast_details.html", podcast=the_podcast)
 
 
